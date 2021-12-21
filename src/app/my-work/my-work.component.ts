d3e89ce6f55fea3436a71d91aa7d6dd2e7b34373
Nathanael Adam <nathanaeladam3@gmail.com>
2021-12-21 13:04:13 +0100

    change the mistakes and make profesional design

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -14,20 +14,24 @@ export class MyWorkComponent implements OnInit {
 
   showAngular = true;
   showJavascript = true;
+  aktiveButton= 1;
 
 
   showAll() {
     this.showAngular = true;
     this.showJavascript = true;
+    this.aktiveButton= 1;
   }
 
   show1(){
     this.showAngular = false;
     this.showJavascript = true;
+    this.aktiveButton = 2;
   }
   
   show2(){
     this.showAngular = true;
     this.showJavascript = false;
+    this.aktiveButton= 3;
   }
 }
